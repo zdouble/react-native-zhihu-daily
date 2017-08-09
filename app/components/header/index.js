ee6019beceeaf404f5913c052f700baf53683ea2
zdouble <872715485@qq.com>
2017-08-09 18:39:20 +0800

    头部30%

--- app/components/header/index.js
+++ app/components/header/index.js
@@ -11,19 +11,42 @@ class Header extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <View style={{paddingLeft: 10}}>
-                    <TouchableOpacity onPress={this.props.openDrawer} style={{ marginTop: -10 }}>
-                        <Image source={require('../../assets/images/abc_textfield_search_activated_mtrl_alpha.9.png')} style={{ width: 36, height: 10 }} />
-                        <Image source={require('../../assets/images/abc_textfield_search_activated_mtrl_alpha.9.png')} style={{ width: 36, height: 10 }} />
-                        <Image source={require('../../assets/images/abc_textfield_search_activated_mtrl_alpha.9.png')} style={{ width: 36, height: 10 }} />
+                <View style={{ paddingLeft: 10 }}>
+                    <TouchableOpacity
+                        onPress={this.props.openDrawer}
+                        style={{ marginTop: -10 }}
+                    >
+                        <Image
+                            source={require('../../assets/images/abc_textfield_search_activated_mtrl_alpha.9.png')}
+                            style={{ width: 36, height: 10 }}
+                        />
+                        <Image
+                            source={require('../../assets/images/abc_textfield_search_activated_mtrl_alpha.9.png')}
+                            style={{ width: 36, height: 10 }}
+                        />
+                        <Image
+                            source={require('../../assets/images/abc_textfield_search_activated_mtrl_alpha.9.png')}
+                            style={{ width: 36, height: 10 }}
+                        />
                     </TouchableOpacity>
                 </View>
                 <View style={{ flex: 1 }}>
-                    <View style={{paddingLeft: 20}}>
-                        <Text style={{color: '#fff', fontSize: 18}}>首页</Text>
+                    <View style={{ paddingLeft: 20 }}>
+                        <Text style={{ color: '#fff', fontSize: 18 }}>首页</Text>
                     </View>
                 </View>
-                <View></View>
+                <View>
+                    <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center' }}>
+                        <Image
+                            source={require('../../assets/images/message.png')}
+                            style={{ width: 40, height: 40 }}
+                        />
+                        <Image
+                            source={require('../../assets/images/abc_ic_menu_moreoverflow_mtrl_alpha.png')}
+                            style={{ width: 40, height: 40 }}
+                        />
+                    </View>
+                </View>
             </View>
         )
     }
